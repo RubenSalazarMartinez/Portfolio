740cc8c4742f5fef5f3c810e1da88695d4ad9c72
darksoultube <darksoultube@gmail.com>
2025-08-26 12:31:49 +0200

    Agregar archivos PHP para las secciones de contacto, experiencia, estudios y sobre mí; eliminar archivo HTML obsoleto de tecnologías.

--- header.php
+++ header.php
@@ -1,16 +1,16 @@
 <header class="header">
-        <div class="img"></div>
-        <h1 class="title">Ruben Salazar Martinez</h1>
+    <div class="img"></div>
+    <h1 class="title">Ruben Salazar Martinez</h1>
 </header>
 <div class="nav">
     <nav>
         <ul>
-            <li><a href="sobreMi/sobreMi.php">Sobre Mi</a></li>
-            <li><a href="proyectos/proyectos.php">Proyectos</a></li>
-            <li><a href="tecnologias/tecnologias.php">Tecnologias</a></li>
-            <li><a href="esperiencia/esperiencia.php">Esperiencia Laboral</a></li>
-            <li><a href="estudios/estudios.php">Estudios</a></li>
-            <li><a href="contacto/contacto.php">Contacto</a></li>
+            <li><a href="/Portfolio/sobreMi/sobreMi.php">Sobre Mi</a></li>
+            <li><a href="/Portfolio/proyectos/proyectos.php">Proyectos</a></li>
+            <li><a href="/Portfolio/tecnologias/tecnologias.php">Tecnologias</a></li>
+            <li><a href="/Portfolio/esperiencia/esperiencia.php">Esperiencia Laboral</a></li>
+            <li><a href="/Portfolio/estudios/estudios.php">Estudios</a></li>
+            <li><a href="/Portfolio/contacto/contacto.php">Contacto</a></li>
         </ul>
     </nav>
 </div>
