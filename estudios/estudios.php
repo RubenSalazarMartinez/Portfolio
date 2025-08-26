3045776de32686a4479a5eb782bdcdbfa0ba00b9
darksoultube <darksoultube@gmail.com>
2025-08-26 13:33:08 +0200

    Pequeños reajustes de las letras y cambios de como se ve el css

--- estudios/estudios.php
+++ estudios/estudios.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="estudios.css">
+    <title>Estudios</title>
 </head>
 <body>
     <?php include("../header.php"); ?>
+    <section class="estudiosZona">
+        <h2 class="estudiosTitulo">Mis Estudios</h2>
+        <div class="estudiosLista">
+            <ul>
+                <li>
+                    <strong>Desarrollo de Aplicaciones Web (DAW)</strong><br>
+                    Ciclo Formativo de Grado Superior<br>
+                    <span class="estudiosFecha">Finalizado</span>
+                </li>
+                <li>
+                    <strong>Inglés Técnico</strong><br>
+                    Formación específica para informática y tecnología<br>
+                    <span class="estudiosFecha">Finalizado</span>
+                </li>
+            </ul>
+        </div>
+    </section>
 </body>
 </html>
--- proyectos/proyectos.php
+++ proyectos/proyectos.php
@@ -1,51 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style.css">
-    <link rel="stylesheet" href="reloj/reloj.css">
-    <link rel="stylesheet" href="contador/contador.css">
-    <link rel="stylesheet" href="generador/generador.css">
-    <link rel="stylesheet" href="proyectos.css">
-    <link rel="stylesheet" href="tresEnRaya/tresEnRaya.css">
-    <script src="reloj/reloj.js"></script>
-    <script src="contador/contador.js"></script>
-    <script src="generador/generador.js"></script>
-    <script src="tresEnRaya/tresEnRaya.js"></script>
-    <title>Document</title>
+    <link rel="stylesheet" href="/Portfolio/style.css">
+    <link rel="stylesheet" href="/Portfolio/proyectos/proyectos.css">
+    <link rel="stylesheet" href="/Portfolio/proyectos/reloj/reloj.css">
+    <link rel="stylesheet" href="/Portfolio/proyectos/contador/contador.css">
+    <link rel="stylesheet" href="/Portfolio/proyectos/generador/generador.css">
+    <link rel="stylesheet" href="/Portfolio/proyectos/tresEnRaya/tresEnRaya.css">
+    <script src="/Portfolio/proyectos/reloj/reloj.js"></script>
+    <script src="/Portfolio/proyectos/contador/contador.js"></script>
+    <script src="/Portfolio/proyectos/generador/generador.js"></script>
+    <script src="/Portfolio/proyectos/tresEnRaya/tresEnRaya.js"></script>
+    <title>Proyectos</title>
 </head>
-<body onload="mostrarReloj()">
-    <?php include '../header.php';?>
-    <section class="proyectoZona">
-        <h2 class="proyectoTitulo">Reloj</h2>
-        <div id="reloj"></div>
-    </section>
-    <section class="proyectoZona">
-        <h2 class="proyectoTitulo">Contador</h2>
-        <div id="contadorContenedor">
-            <div id="contadorTiempo">00:00:00</div>
-            <div id="contadorBotones">
-                <button id="iniciarBtn">Iniciar</button>
-                <button id="pararBtn">Parar</button>
-                <button id="reiniciarBtn">Reiniciar</button>
+<body>
+    <?php include("../header.php"); ?>
+
+    <div class="proyectosFila">
+        <section class="proyectoZona">
+            <h2 class="proyectoTitulo">Reloj</h2>
+            <div id="reloj"></div>
+        </section>
+        <section class="proyectoZona">
+            <h2 class="proyectoTitulo">Contador</h2>
+            <div id="contadorContenedor">
+                <div id="contadorTiempo">00:00:00</div>
+                <div id="contadorBotones">
+                    <button id="iniciarBtn">Iniciar</button>
+                    <button id="pararBtn">Parar</button>
+                    <button id="reiniciarBtn">Reiniciar</button>
+                </div>
             </div>
-        </div>
-    </section>
-    <section class="proyectoZona">
-        <h2 class="proyectoTitulo">Generador de Contraseñas</h2>
-        <div id="generadorContenedor">
-            <input type="text" id="contraseñaGenerada" readonly placeholder="Tu contraseña aparecerá aquí">
-            <button id="generarBtn">Generar contraseña</button>
-        </div>
-    </section>
-    <section class="proyectoZona">
-        <h2 class="proyectoTitulo">Tres en Raya</h2>
-        <div id="tresEnRayaContenedor">
-            <div id="tresEnRayaTablero"></div>
-            <div id="tresEnRayaInfo"></div>
-            <button id="tresEnRayaReiniciarBtn">Reiniciar</button>
-        </div>
-    </section>
+        </section>
+        <section class="proyectoZona">
+            <h2 class="proyectoTitulo">Generador de Contraseñas</h2>
+            <div id="generadorContenedor">
+                <input type="text" id="contraseñaGenerada" readonly placeholder="Tu contraseña aparecerá aquí">
+                <button id="generarBtn">Generar contraseña</button>
+            </div>
+        </section>
+    </div>
+
+    <div class="proyectosFila">
+        <section class="proyectoZona">
+            <h2 class="proyectoTitulo">Tres en Raya</h2>
+            <div id="tresEnRayaContenedor">
+                <div id="tresEnRayaTablero"></div>
+                <div id="tresEnRayaInfo"></div>
+                <button id="tresEnRayaReiniciarBtn">Reiniciar</button>
+            </div>
+        </section>
+    </div>
 </body>
 </html>
